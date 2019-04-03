af86755e0a9d6bb0bf338a784ac64e2268d5dcec
karlactm <karlactabosa@gmail.com>
2019-04-03 10:19:44 -0300

    Update create_bd.pl

--- create_bd.pl
+++ create_bd.pl
@@ -13,7 +13,7 @@ for (my $i = 0; $i < scalar (@lineages); $i++) {
         my $turn = $rand [$j];
         my $arg = $dir."/".$lineages [$i]."/".$turn."/output_find_homologous/"; 
         system ("mkdir $arg/output_peptide_trip/");
-        system("./pep_trip.pl " . $arg);
+        system("./peptide_trip.pl " . $arg);
         $arg = undef;       
    }        
 }
